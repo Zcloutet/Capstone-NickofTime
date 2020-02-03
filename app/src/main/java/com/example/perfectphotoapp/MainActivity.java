07f9386c2779df5db1222b5ce46606f5e2e03ccd
Samuel Ehlmann <samuelehlmann@gmail.com>
2020-02-03 15:10:19 -0600

    opening camera
    openCamera and stateCallback

--- app/src/main/java/com/example/perfectphotoapp/MainActivity.java
+++ app/src/main/java/com/example/perfectphotoapp/MainActivity.java
@@ -1,10 +1,45 @@
 package com.example.perfectphotoapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import android.hardware.camera2.*;
+
 public class MainActivity extends AppCompatActivity {
+    protected String cameraId;
+    protected CameraDevice cameraDevice;
+
+    private final CameraDevice.StateCallback stateCallBack = new CameraDevice.StateCallback() {
+        @Override
+        public void onOpened(@NonNull CameraDevice camera) {
+            cameraDevice = camera;
+        }
+
+        @Override
+        public void onDisconnected(@NonNull CameraDevice camera) {
+            cameraDevice.close();
+        }
+
+        @Override
+        public void onError(@NonNull CameraDevice camera, int error) {
+            cameraDevice.close();
+        }
+    };
+
+    private void openCamera() {
+        CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+            cameraId = manager.getCameraIdList()[0];
+
+            manager.openCamera(cameraId, stateCallBack, null);
+        }
+        catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
