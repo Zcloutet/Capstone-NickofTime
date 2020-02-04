7943d3a0e93fe8ffb055218a533d23f44eb1f301
Samuel Ehlmann <samuelehlmann@gmail.com>
2020-02-04 01:22:22 -0600

    comments & log messages

--- app/src/main/java/com/example/perfectphotoapp/MainActivity.java
+++ app/src/main/java/com/example/perfectphotoapp/MainActivity.java
@@ -18,11 +18,15 @@ import android.animation.ValueAnimator;
 import android.hardware.camera2.*;
 
 public class MainActivity extends AppCompatActivity {
+    // variables refering to the camera
     protected String cameraId;
     protected CameraDevice cameraDevice;
 
+    // tag for logging
     private static final String TAG = "PerfectPhoto";
 
+    // stateCallBack for opening cameras
+    // not necessarily important to use but rather than make it null, it is here
     private final CameraDevice.StateCallback stateCallBack = new CameraDevice.StateCallback() {
         @Override
         public void onOpened(@NonNull CameraDevice camera) {
@@ -43,27 +47,33 @@ public class MainActivity extends AppCompatActivity {
     private void takePhoto() {
 
         // play shutter sound
+        Log.i(TAG, "playing shutter sound");
         MediaActionSound mediaActionSound = new MediaActionSound();
         mediaActionSound.play(MediaActionSound.SHUTTER_CLICK);
 
         // flash screen
+        Log.i(TAG, "flashing screen");
         final ImageView flash = findViewById(R.id.imageViewFlash);
+        // create animator to make flash pleasant
         ValueAnimator flashAnimator = ValueAnimator.ofInt(255,0);
         flashAnimator.setInterpolator(new AccelerateInterpolator());
         flashAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
+                // update the imageview's alpha to let it fade out
                 flash.setImageAlpha((int) animation.getAnimatedValue());
             }
         });
         flashAnimator.addListener(new Animator.AnimatorListener() {
             @Override
             public void onAnimationStart(Animator animation) {
+                // make it visible when it starts
                 flash.setVisibility(View.VISIBLE);
             }
 
             @Override
             public void onAnimationEnd(Animator animation) {
+                // make it gone to save resources when the animation is over
                 flash.setVisibility(View.GONE);
             }
 
@@ -76,18 +86,23 @@ public class MainActivity extends AppCompatActivity {
             public void onAnimationRepeat(Animator animation) {
             }
         });
+        // the duration can be tuned
         flashAnimator.setDuration(700);
+        // animate it
         flashAnimator.start();
     }
 
     private void openCamera() {
+        // open camera by getting camera manager and opening the first camera
         CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
         try {
             cameraId = manager.getCameraIdList()[0];
 
+            // check for permission here
             manager.openCamera(cameraId, stateCallBack, null);
         }
         catch (CameraAccessException e) {
+            // if there was a problem accessing the camera, let the user know
             e.printStackTrace();
             Toast.makeText(getApplicationContext(),"Error opening camera.",Toast.LENGTH_SHORT).show();
         }
@@ -95,6 +110,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void closeCamera() {
+        // close the camera, if one is open
         if (cameraDevice != null) {
             cameraDevice.close();
             cameraDevice = null;
@@ -112,12 +128,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        openCamera();
+        openCamera(); // open camera whenever the app is opened
     }
 
     @Override
     protected void onPause() {
-        closeCamera();
+        closeCamera(); // close camera whenever the app is no longer open
         super.onPause();
     }
 }
