38417e69df90ed766b45f5819ae0ca9631cc2cfa
Bibhut Khadka <bibhutkhadka123@gmail.com>
2020-03-19 19:27:17 +0545

    modified gitignore

---
+++ app/src/main/java/com/example/perfectphotoapp/GalleryActivity.java
@@ -0,0 +1,28 @@
+package com.example.perfectphotoapp;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.ImageView;
+
+public class GalleryActivity extends AppCompatActivity {
+
+    ImageView selectedImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gallery);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+}
