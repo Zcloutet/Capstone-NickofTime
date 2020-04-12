86f5757ceaa7697321b8e0664cc9e226712f2ddf
Nirjala123 <parajulinirjala@gmail.com>
2020-04-12 13:48:22 -0500

    feat: add session to save eye detection

--- app/src/main/java/com/example/perfectphotoapp/SettingsActivity.java
+++ app/src/main/java/com/example/perfectphotoapp/SettingsActivity.java
@@ -3,11 +3,24 @@ package com.example.perfectphotoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.ImageButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private Switch eyeDetection;
+    private Switch smileDetection;
+
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String EYESWITCH = "EYESWITCH";
+    public static final String SMILESWITCH = "SMILESWITCH";
+
+    private boolean eyeSwitchOnOff;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +33,35 @@ public class SettingsActivity extends AppCompatActivity {
                 goBack();
             }
         });
+
+        eyeDetection = (Switch) findViewById(R.id.eyeDetection);
+        smileDetection = (Switch) findViewById(R.id.smileDetection);
+
+        eyeDetection.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                saveEyeDetectionMode();
+            }
+        });
+
+        loadData();
+        updateSwitches();
+    }
+    public void saveEyeDetectionMode(){
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean(EYESWITCH, eyeDetection.isChecked());
+
+        editor.apply();
+//        Toast.makeText(this, "MODE CHANGED", Toast.LENGTH_SHORT).show();
+    }
+
+    public void loadData(){
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        eyeSwitchOnOff = sharedPreferences.getBoolean(EYESWITCH, true);
+    }
+    public void updateSwitches(){
+        eyeDetection.setChecked(eyeSwitchOnOff);
     }
 
     public void goBack(){
